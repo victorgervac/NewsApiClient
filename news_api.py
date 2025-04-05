6de1de55f5e068c250b22a60f49fbd326dc11dc0
Victor <62909694+victorgervac@users.noreply.github.com>
2025-04-05 17:22:07 -0600

    Update news_api.py

--- news_api.py
+++ news_api.py
@@ -2,10 +2,9 @@ from newsapi import NewsApiClient
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
+
 load_dotenv()
 
-# Access your API key
 api_key = os.getenv("NEWS_API_KEY")
 
 newsapi = NewsApiClient(api_key=api_key)
